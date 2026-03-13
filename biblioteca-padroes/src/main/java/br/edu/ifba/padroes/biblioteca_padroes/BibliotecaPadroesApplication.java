6fee72b0037b5a9b2dc22a87a2d244e248916c12
lucasif18 <126366824+lucasif18@users.noreply.github.com>
2026-03-13 00:11:29 -0300

    Atualização

---
+++ biblioteca-padroes/src/main/java/br/edu/ifba/padroes/biblioteca_padroes/BibliotecaPadroesApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifba.padroes.biblioteca_padroes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaPadroesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaPadroesApplication.class, args);
+	}
+
+}
---
+++ biblioteca-padroes/src/test/java/br/edu/ifba/padroes/biblioteca_padroes/BibliotecaPadroesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifba.padroes.biblioteca_padroes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaPadroesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
